776c1108de8a0700788ee42e8cc1eb414e57c0cb
Bbillionaires <88366222+Bbillionaires@users.noreply.github.com>
2026-06-13 15:00:55 -0400

    Mirror egrowthnow-ui codebase to egrowthnew

---
+++ src/app/blog-details/page.tsx
@@ -0,0 +1,54 @@
+import SharePost from "@/components/Blog/SharePost";
+import TagButton from "@/components/Blog/TagButton";
+import Image from "next/image";
+
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "eGrowthNow",
+  description: "eGrowthNow",
+  // other metadata
+};
+
+const BlogDetailsPage = () => {
+  return (
+    <>
+      <section className="pb-[120px] pt-[150px]">
+        <div className="container">
+          <div className="-mx-4 flex flex-wrap justify-center">
+            <div className="w-full px-4 lg:w-8/12">
+              <div>
+                <h2 className="mb-8 text-3xl font-bold leading-tight text-black dark:text-white sm:text-4xl sm:leading-tight">
+                  10 amazing sites to download stock photos & digital assets for
+                  free
+                </h2>
+                <div className="items-center justify-between sm:flex">
+                  <div className="mb-5">
+                    <h4 className="mb-3 text-sm font-medium text-body-color">
+                      Popular Tags :
+                    </h4>
+                    <div className="flex items-center">
+                      <TagButton text="Design" />
+                      <TagButton text="Development" />
+                      <TagButton text="Info" />
+                    </div>
+                  </div>
+                  <div className="mb-5">
+                    <h5 className="mb-3 text-sm font-medium text-body-color sm:text-right">
+                      Share this post :
+                    </h5>
+                    <div className="flex items-center sm:justify-end">
+                      <SharePost />
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+    </>
+  );
+};
+
+export default BlogDetailsPage;
---
+++ src/app/blog-sidebar/page.tsx
@@ -0,0 +1,39 @@
+import RelatedPost from "@/components/Blog/RelatedPost";
+import SharePost from "@/components/Blog/SharePost";
+import TagButton from "@/components/Blog/TagButton";
+import NewsLatterBox from "@/components/Contact/NewsLatterBox";
+import Image from "next/image";
+
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "eGrowthNow",
+  description: "eGrowthNow",
+  // other metadata
+};
+
+const BlogSidebarPage = () => {
+  return (
+    <>
+      <section className="overflow-hidden pb-[120px] pt-[180px]">
+        <div className="container">
+          <div className="-mx-4 flex flex-wrap">
+            <div className="w-full px-4 lg:w-8/12">
+              <div>
+                <h1 className="mb-8 text-3xl font-bold leading-tight text-black dark:text-white sm:text-4xl sm:leading-tight">
+                  10 amazing sites to download stock photos & digital assets for
+                  free
+                </h1>
+              </div>
+            </div>
+            <div className="w-full px-4 lg:w-4/12">
+              <NewsLatterBox />
+            </div>
+          </div>
+        </div>
+      </section>
+    </>
+  );
+};
+
+export default BlogSidebarPage;
